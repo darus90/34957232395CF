cc2631fcd657e60b6929738f235d9b0d9b7c716d
darus90 <33200735+darus90@users.noreply.github.com>
2025-06-11 18:10:52 -0500

    Add files via upload

---
+++ Release/index.js
@@ -0,0 +1,15 @@
+module.exports.html = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Mi Página</title>
+  <link rel="stylesheet" href="/static/style.css">
+</head>
+<body>
+  <h1>Bienvenido</h1>
+  <img src="/static/logo.png" alt="Logo">
+</body>
+</html>
+`;
+
